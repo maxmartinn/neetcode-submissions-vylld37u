d40c296d2bcc69b727c49847c4fb0c894d4fd859
Max Martin <56135027+maxmartinn@users.noreply.github.com>
2026-04-16 11:16:14 -0400

    Add: sliding-window-maximum - submission-7

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-7.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        maxHeap = []
+        for i in range(k - 1):
+            heapq.heappush(maxHeap, (-nums[i], i))
+        for i in range(k - 1, len(nums)):
+            heapq.heappush(maxHeap, (-nums[i], i))
+            while not (i - k + 1 <= maxHeap[0][1] <= i):
+                heapq.heappop(maxHeap)
+            res.append(-maxHeap[0][0])
+        print(res)
+        return res
+
+        """
+
+        Input: nums = [1,2,1,0,4,2,6], k = 3
+        res = [2, 2, 4, 4]
+        Output: [2,2,4,4,6]
+
+        maxHeap = [(-6, 4), (-4, 4), (-2, 1), (-2, 4) (-1, 0), (-1, 0), (0, 2)]
+
+        Explanation:
+        Window position            Max
+        ---------------           -----
+        [1  2  1] 0  4  2  6        2
+        1 [2  1  0] 4  2  6        2
+        1  2 [1  0  4] 2  6        4
+        1  2  1 [0  4  2] 6        4
+        1  2  1  0 [4  2  6]       6
+
+        """
+
+        # naive solution
+
+        # using a heap store ((-) value, index)
+
+        # add first k - 1 values to the heap
+
+        # start from index k - 1:
+
+        # add to heap
+        # try to get maximum untill the current value is inside i - k + 1 <= index <= i
+    
